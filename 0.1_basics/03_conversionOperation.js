14277660215c46bd2c51a6a1250e799d80e90db2
Halee-stack <haleesajj@gmail.com>
2026-05-16 14:07:13 +0000

    conversion of datatypes

---
+++ 0.1_basics/03_conversionOperation.js
@@ -0,0 +1,88 @@
+let marks = 25
+//console.log(typeof marks);
+//console.log(typeof(marks));
+
+let valueInNumber = Number(marks)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+//Below are the other Data Types:
+
+
+//STRING
+let rating = "25"
+//console.log(typeof rating);
+//console.log(typeof(rating));
+
+let valueInNumber = Number(rating)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+let points = "Haleema"
+//console.log(typeof points);
+//console.log(typeof(points));
+
+let valueInNumber = Number(points)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//UNDEFINED
+let score = null
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//NaN
+let rank = "2hut3" // when you print this in the terminal this will give you NaN value due to this is Not a Number.
+//console.log(typeof rank);
+//console.log(typeof(rank));
+
+let valueInNumber = Number(rank)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+NULL
+let grade = null
+//console.log(typeof grade);
+//console.log(typeof(grade));
+
+let valueInNumber = Number(grade)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//BOOLEAN
+let score = true
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+//BOOLEAN
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+//if the boolean has 1 or any value in the string it will show TRUE otheriwise false when there is empty string.*/
+
+//is it number or string
+let someNumber = 25
+let stringNumber = String(someNumber)
+//console.log(stringNumber); // when print this will show the number 
+//console.log(typeof stringNumber); // it will show a string in the terminal.
+
+/* As seen above
+"25" => 25 string
+"2hut3" => NaN
+boolean => true or false; 
+true =>1 and false => 0
+"" => false
+"haleema" => true
+*/
+
