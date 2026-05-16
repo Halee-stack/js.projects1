55b29f5515aeb1abf02f3889f0ea5abb93761121
Halee-stack <haleesajj@gmail.com>
2026-05-16 12:53:44 +0000

    container config done

---
+++ 0.1_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use_strict"; 
+// in JavaScript it enables Strict Mode and it treats all JS code as newer version.
+// alert( 5 + 7 ) // aleet can t be defined in ther terminal because we are using node.js not browser.
+
+console.log (3 +3)
+console.log("Haleema")
+// in coding the code readability is very important. it doesnt matter if you code can run if its can't be read then thats not good practice.
+let name = "Haleema"
+let age = 23
+let isLoggedIn = false
+//There are two types of data types: primitive and non- primitive data types.
+// primitive data types are also known as built-in data types: number, strings, boolean, undefined, bigint, symbol and null.
+// non-primitive data types are derived from primitive data types wich are: objects, functions and arrays.  
+console.log(typeof undefined);
+console.log(typeof 3);
+console.log(typeof true);
+console.log(typeof "hello");
+
