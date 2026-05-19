bd9934de5f331fa10be285a029b8c64e27c37d0d
Halee-stack <haleesajj@gmail.com>
2026-05-19 17:47:06 +0000

    Math

---
+++ 0.1_basics/math.js
@@ -0,0 +1,21 @@
+console.log(Math);
+
+console.log(Math.abs(-2));
+console.log(Math.round(5.4));
+console.log(Math.ceil(3.2)); // takes the highest value
+console.log(Math.floor(5.8)); //takes the lowest value
+
+console.log(Math.min(2, 3, 4, 5)); // always output the minimum value
+console.log(Math.max(9, 8, 7, 6)); // always output the maximum value
+
+console.log(Math.random());
+console.log(Math.random()*10);
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 5
+const max = 15
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+
