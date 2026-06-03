fc5fd12ed51b3c1bd108dc21ede939977a5b4df1
Halee-stack <haleesajj@gmail.com>
2026-06-03 22:03:31 +0000

    while loop / do while loop

---
+++ 0.5_iterations/while_loop.js
@@ -0,0 +1,22 @@
+// WHILE LOOP
+
+let index = 0
+while (index <= 15) {
+    console.log(`The value of index is ${index}`);
+    index = index + 3
+}
+
+let myArray = ["spiderman", "thor", "hulk", "iron man"]
+let arr = 0
+while (arr < myArray.length) {
+    console.log(`Value is ${myArray[arr]}`);
+    arr = arr + 1
+}
+
+///DO WHILE LOOP
+let point = 10
+do {
+    console.log(`You have ${point} point left.`);
+    point++
+
+} while (point <= 9);
